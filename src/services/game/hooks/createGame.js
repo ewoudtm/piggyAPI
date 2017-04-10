767c231dc90f3c05172946a9239f176367eb188b
Wouter de Vos <wouter@codaisseur.com>
2017-04-10 23:51:28 +0200

    Set the current user as the first player

--- src/services/game/hooks/createGame.js
+++ src/services/game/hooks/createGame.js
@@ -14,5 +14,7 @@ module.exports = function(options) {
     const currentUser = hook.params.user;
 
     hook.data.title = `${currentUser.name}'s Game`;
+
+    hook.data.playerIds = [hook.params.user._id];
   };
 };
