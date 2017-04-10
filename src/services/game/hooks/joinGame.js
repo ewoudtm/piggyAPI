3dba81bd775770aed4e97eea44e0987fe65830cd
Wouter de Vos <wouter@codaisseur.com>
2017-04-11 00:20:20 +0200

    A crude implementation for the joinGame hook

--- src/services/game/hooks/joinGame.js
+++ src/services/game/hooks/joinGame.js
@@ -5,12 +5,20 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
 
-const defaults = {};
+const errors = require('feathers-errors');
 
 module.exports = function(options) {
-  options = Object.assign({}, defaults, options);
-
   return function(hook) {
-    hook.joinGame = true;
-  };
-};
+    return hook.app.service('games').get(hook.id)
+      .then((game) => {
+        if (hook.data.joinGame === undefined) {
+          throw new errors.Forbidden('You must be the author to change a game like that.');
+        }
+
+        const action = hook.data.joinGame ? '$addToSet' : '$pull';
+        let data = {};
+        data[action] = { playerIds: hook.params.user._id };
+        hook.data = data;
+      })
+  }
+}
