849c07c0f84310756b55fcb31e5b3765aa49dd2a
ewoudtm <ewoud@ewoudtm.nl>
2017-04-14 10:24:33 +0200

    stuff with dieRoll

--- src/services/game/hooks/dieRoll.js
+++ src/services/game/hooks/dieRoll.js
@@ -10,9 +10,20 @@ const defaults = {};
 
 module.exports = function(options) {
   options = Object.assign({}, defaults, options);
-debugger
-  return function(hook) {
 
-    hook.data.dieRoll = dieRoll();
-  };
+  return function(hook) {
+    const rolledDie = dieRoll();
+
+    if (rolledDie >= 2 && rolledDie <= 6) {
+      return hook.app.service('games').get(hook.id)
+        .then(game => {
+          const player = game.players.filter(function(obj){
+            obj.playerId === hook.params.user._id;
+          })[0]; //array
+          player.roundTotal = player.roundTotal + rolledDie;
+          game.players = Object.assign({},game.players,player);
+          return hook;
+        });
+    };
+    hook.data.dieRoll = rolledDie;
 };
